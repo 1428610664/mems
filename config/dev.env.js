bfbed856b2ec8216371943a31d1e61d65d0edc5e
hz <chenhz@coreware.com.cn>
2018-07-11 16:05:05 +0800

    配置文件修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API: '"http://192.168.1.130"'
+  API: '"http://emsapp.coreware.cn"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API: '"http://192.168.1.130"'
+  API: '"http://emsapp.coreware.cn"'
 }
